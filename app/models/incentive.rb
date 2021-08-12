8be457c6056f46206292295dbd72b1ce2bfb278f
Andrew Casarsa <acasarsa@gmail.com>
2021-08-12 15:54:02 -0400

    Add validation for no saving if insentive is redeemed

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,2 +1,12 @@
 class Incentive < ApplicationRecord
+  validate :no_save_if_redeemed
+  validates_presence_of :code
+
+  private 
+
+  def no_save_if_redeemed
+    if self.redeemed == true
+      self.errors.add(:base, "Incentive has been redeemed and cannot be updated")
+    end
+  end
 end
